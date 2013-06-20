0c9e9c0d12ef6e95c69997dbfa1c6840e19f4bd4
Skye <snschnei@oakland.edu>
2013-06-20 13:21:46 -0400

    fixed some formating issues and added comments to app.js

--- src/js/src/app.js
+++ src/js/src/app.js
@@ -115,9 +115,12 @@ var umobile = {
 			// Iterate over portlets.
 			_.each(portlets, function (portlet, idx) {
 				portlet.id = portlet.fname;
-                function grabIcon(){
-                    var imagePath;
-                    var url = config.uMobileServerUrl + portlet.iconUrl;
+                
+                // Dynamically download Icon from the server
+                // using the iconUrl from the JSON feed.
+                function grabIcon() {
+                    var imagePath; // path to where the image will be saved.
+                    var url = config.uMobileServerUrl + portlet.iconUrl; // url where the image can be found.
                     window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
                         imagePath = fs.root.fullPath + portlet.iconUrl;
                         var fileTransfer = new FileTransfer();
@@ -126,29 +129,31 @@ var umobile = {
                             url,
                             imagePath,
                         function (entry) {
-                                console.log("downoad complete: " + entry.fullPath);
+                                console.log('downoad complete: ' + entry.fullPath);
                             },
                         function (error) {
-                                console.log("download error source " + error.source);
-                                console.log("download error target " + error.target);
-                                console.log("upload error code " + error.code);
+                                console.log('download error source ' + error.source);
+                                console.log('download error target ' + error.target);
+                                console.log('upload error code ' + error.code);
                             }
                         );
-                    })
-                    return imagePath; 
+                    });
+                    return imagePath; // return image path so it can be found again 
                 }
+
 				// Parse the config.nativeIcons object for a property
 				// that matches the portlet.fname. If one is found, set
 				// the icon url to leverage a locally stored icon. If a
 				// match is not found, set the icon url to an icon on the
 				// server.
-				if (config.nativeIcons[portlet.fname]) {
-					portlet.iconUrl = 'images/icons/' + config.nativeIcons[portlet.fname];
-				} else {
-                    var path = grabIcon();
-                    console.log("PATH ========= " + path);
-					portlet.iconUrl = path;
-				}
+
+                // Commented out the check for local image becuause we will be pulling our images from the server.
+				//if (config.nativeIcons[portlet.fname]) {
+				//portlet.iconUrl = 'images/icons/' + config.nativeIcons[portlet.fname];
+				//} else {
+                var path = grabIcon(); // grap path of where the image was saved.
+                portlet.iconUrl = path;
+				//}
 
 				// Parse the config.nativeModules object for a property that
 				// matches the portlet.fname. If one is found, the module or
